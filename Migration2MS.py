c892b3f0b78fb0e014dac36d74496cbc5ce19b24
Vladimir Shchur <vlshchur@gmail.com>
2018-07-18 17:55:27 -0700

    migration2ms

--- Migration2MS.py
+++ Migration2MS.py
@@ -20,6 +20,7 @@ clargs = parser.parse_args()
 #fmigr  = os.path.join( clargs.wd, clargs.fmigr  )
 
 data = migrationIO.ReadMigration(clargs.fmigr, False)
+scaling = Migration.SetScaling()
 #data = MigData(splitT = data[0], migStart = data[1], migEnd = data[2], times = data[3], lambda1 = data[4], lambda2 = data[5], thrh = data[6])
 numT = len(data.times)
 
@@ -27,23 +28,26 @@ chromLen = 3000000
 chromNum = 1000
 binSize = 100
 
-theta = 2*chromLen*data.thrh[0]/binSize
-rho = 2*chromLen*data.thrh[1]/binSize
+
+N0 = data.thrh[0]/(2*scaling[0]*scaling[1])
+N0_rescale = 10000/N0
+theta = 2*chromLen*data.thrh[0]/binSize*N0_rescale
+rho = 2*chromLen*data.thrh[1]/binSize*N0_rescale
 
 mscl = " 4 " + str(chromNum) + " -t " + str(theta) + " -r " + str(rho) + " " + str(chromLen) + " -l -I 2 2 2 "
 for i in range(data.splitT):
-    mscl += " -en " + str(data.times[i]/2.0) + " 1 " + str(1.0/data.lambda1[i])
-    mscl += " -en " + str(data.times[i]/2.0) + " 2 " + str(1.0/data.lambda2[i])
+    mscl += " -en " + str(data.times[i]/2.0/N0_rescale) + " 1 " + str(N0_rescale/data.lambda1[i])
+    mscl += " -en " + str(data.times[i]/2.0/N0_rescale) + " 2 " + str(N0_rescale/data.lambda2[i])
 
-mscl += " -em " + str(data.times[ data.migStart ]/2.0) + " 1 2 " + str(2*data.mu[0])
-mscl += " -em " + str(data.times[ data.migStart ]/2.0) + " 2 1 " + str(2*data.mu[1])
-mscl += " -eM " + str(data.times[ data.migEnd ]/2.0) + " 0.0 "
+mscl += " -em " + str(data.times[ data.migStart ]/2.0/N0_rescale) + " 1 2 " + str(2*data.mu[0]*N0_rescale)
+mscl += " -em " + str(data.times[ data.migStart ]/2.0/N0_rescale) + " 2 1 " + str(2*data.mu[1]*N0_rescale)
+mscl += " -eM " + str(data.times[ data.migEnd ]/2.0/N0_rescale) + " 0.0 "
 
-mscl += " -ej " + str(data.times[ data.splitT ]/2.0) + " 2 1 "
-mscl += " -eM " + str(data.times[ data.splitT ]/2.0) + " 0.0 "
+mscl += " -ej " + str(data.times[ data.splitT ]/2.0/N0_rescale) + " 2 1 "
+mscl += " -eM " + str(data.times[ data.splitT ]/2.0/N0_rescale) + " 0.0 "
 
 for i in range(data.splitT, numT):
-    mscl += " -eN " + str(data.times[i]/2.0) + " " + str(1.0/data.lambda1[i])
+    mscl += " -eN " + str(data.times[i]/2.0/N0_rescale) + " " + str(N0_rescale/data.lambda1[i])
 
 print(mscl)
 
