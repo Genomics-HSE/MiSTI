d97f291e1277a114e76710e8ca4ec4b747755301
Vladimir Shchur <vlshchur@gmail.com>
2018-07-20 19:30:36 -0700

    debug

--- TwoPopulations.py
+++ TwoPopulations.py
@@ -287,7 +287,7 @@ class TwoPopulations:
             self.UpdateLineagePop(state1, i, (state[i].pop+1)%2)
             ind2 = self.MapStateToInd(state1)
             MM[ind2][ind] += self.mu[ 1-state[i].pop ]
-            total += self.mu[ state[i].pop ]
+            total += self.mu[ 1-state[i].pop ]
             #Coalescence
             for j in range(i+1, len(state)):
                 if state[j].pop != state[i].pop:
