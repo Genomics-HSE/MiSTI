5d62a5d3882187d1289e4e83407fb66eb8b19d11
Vladimir Shchur <vlshchur@gmail.com>
2018-08-06 16:51:34 -0700

    tt method (Schlebusch et al, Genetics 2017)

--- ttmethod.py
+++ ttmethod.py
@@ -16,7 +16,17 @@ parser.add_argument('jafs',
 parser.add_argument('haplen',
                     help='Halotype length (total number of sites, both variable and non-variable)')
 
+parser.add_argument('-y', nargs=1, type=float, default=30,
+                    help='years per generation')
+parser.add_argument('-mu', nargs=1, type=float, default=1.25e-8,
+                    help='mutation rate (per basepair per generation)')
+
 clargs = parser.parse_args()
+
+if isinstance(clargs.y, list):
+    clargs.y = clargs.y[0]
+if isinstance(clargs.fout, list):
+    clargs.mu = clargs.mu[0]
 
 spectrum = ReadJAFS(clargs.jafs)
 spectrum = spectrum[1:]
@@ -33,8 +43,8 @@ m7 = spectrum[6]
 T1 = (m1/2+m3-(2*m6+m5)*(6*m7+m5)/8/m5)/M
 T2 = (m2/2+m4-(2*m7+m5)*(6*m6+m5)/8/m5)/M
 
-mu = 1.25e-8
-Ygen = 30
+mu = clargs.mu
+Ygen = clargs.y
 T1y = T1/mu*Ygen
 T2y = T2/mu*Ygen
 
