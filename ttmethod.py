de26a8a21d6b7106e21a219ee769750f14a7f44d
Vladimir Shchur <vlshchur@gmail.com>
2018-08-10 18:58:38 -0700

    tt method: population sizes, credits

--- ttmethod.py
+++ ttmethod.py
@@ -2,12 +2,10 @@
 
 import sys
 import os
-import collections
 import argparse
 import numpy
-import math
 from migrationIO import ReadJAFS
-
+from math import log
 
 parser = argparse.ArgumentParser(description='Implementation of TT-method (Schlebusch et al, Genetics 2017)')
 
@@ -43,10 +41,24 @@ m7 = spectrum[6]
 T1 = (m1/2+m3-(2*m6+m5)*(6*m7+m5)/8/m5)/M
 T2 = (m2/2+m4-(2*m7+m5)*(6*m6+m5)/8/m5)/M
 
+a1 = 2*m5/(2*m6+m5)
+a2 = 2*m5/(2*m7+m5)
+
+theta = 3/M*(2*m6+m5)*(2*m7+m5)/(8*m5)/2
+theta1 = -T1/log(a1)/2
+theta2 = -T2/log(a2)/2
+
 mu = clargs.mu
 Ygen = clargs.y
 T1y = T1/mu*Ygen
 T2y = T2/mu*Ygen
 
+N_A = theta/mu
+N_1 = theta1/mu
+N_2 = theta2/mu
+
+print("Implementation of tt method (Schlebusch et al, Genetics 2017)")
 print("T1 = ", T1y)
 print("T2 = ", T2y)
+print("N_A = ", N_A, "\tN_1 = ", N_1, "\tN_2 = ", N_2)
+
