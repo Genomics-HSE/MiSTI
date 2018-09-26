0ecad72e00c8ce949fe7532be38d47f425b4bdb0
Vladimir Shchur <vlshchur@gmail.com>
2018-09-25 18:00:11 -0700

    License and renaming

--- migrationIO.py
+++ migrationIO.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
-#Copyright (c) 2018 Vladimir Shchur (vlshchur@gmail.com)
+#    Copyright (c) 2018 Vladimir Shchur (vlshchur@gmail.com)
+#
+#    This file is part of MiSTI.
+#
+#    MiSTI is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    MiSTI is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with MiSTI.  If not, see <https://www.gnu.org/licenses/>.
+
 
 import sys
 import matplotlib.pyplot as plt
