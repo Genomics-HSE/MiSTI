d8dc7ba06d0d7bea5617b40946c6a7efb4fb4df5
ekrez <84871963+ekrez@users.noreply.github.com>
2025-10-07 15:55:30 +0300

    Add files via upload

--- prep_data/hpc/make.2d.sfs.sh
+++ prep_data/hpc/make.2d.sfs.sh
@@ -5,6 +5,13 @@
 nthr=$1
 sample1=$2
 sample2=$3
+pop1=$4
+pop2=$5
 
-realSFS  ./${sample1}.sfs/${sample1}.all.saf.idx   ./${sample2}.sfs/${sample2}.all.saf.idx -cores ${nthr}  -win 2500000 -step 2500000  > ${sample1}_${sample2}.2dsfs             
- 
+
+mkdir -p ./2dsfs
+realSFS  ./${sample1}/${sample1}.sfs/${sample1}.all.saf.idx   ./${sample2}/${sample2}.sfs/${sample2}.all.saf.idx -cores ${nthr}  -nSites 2500000  > ./2dsfs/${sample1}_${sample2}.new.2dsfs             
+
+wait
+
+./utils/ANGSDSFS.py ./2dsfs/${sample1}_${sample2}.new.2dsfs ${pop1} ${pop2} > ./2dsfs/${sample1}_${sample2}.new.sfs 
